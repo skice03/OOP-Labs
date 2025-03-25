45d47e3aec4a980b1492efde9f550b2539902f7b
skice03 <marinelcipu21@gmail.com>
2025-03-25 15:30:50 +0200

    modificari

--- lab1_modular_programming/Problema1.2/main.c
+++ lab1_modular_programming/Problema1.2/main.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
-#define USE_HEADER1 1
-#if USE_HEADER1==1
-    #include "header1.h"
-#else   
-    #include "header2.h"
-#endif
 
+#define DEBUG // pentru cerinta de la problema 1.3
+
+#define USE_HEADER1 1
+#if USE_HEADER1 == 1
+#include "header1.h"
+#else
+#include "header2.h"
+#endif
 
 int main()
 {
     int x, y;
     printf("Enter the values of X and Y: ");
     scanf("%d %d", &x, &y);
-    printf("%d\n", f(x,y));
+    printf("%d\n", f(x, y));
+
+#ifdef DEBUG
+    printf("Se foloseste modul debug");
+#endif
 
     return 0;
 }
